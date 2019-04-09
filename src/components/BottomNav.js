3f5581358a4ec1d0fa8af252852b20420c5f3e69
Glitch (create-react-app-sample) <none>
2019-04-09 18:45:12 +0000

    👜🏣 Checkpoint ./src/components/BottomNav.js:1424713/601 ./src/theme.js:1424713/396 ./src/index.js:1424713/467 ./watch.json:1424713/1 ./package.json:1424713/1029

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -5,16 +5,28 @@ import Icon from '@material-ui/core/Icon'
 import React, { Component } from 'react'
 
 const iconHome = (
-  <Icon fontSize={ "large" }>home</Icon>
+  <Icon color={ "primary" }>home</Icon>
 )
 
 class BottomNav extends Component {
+  iconHome() {
+    return (
+      <Icon color={ this.props.active === "home" ? "primary" : "default" }>home</Icon>
+    )
+  }
+
+  homeNavigationButton() {
+    return (
+      <BottomNavigationAction label={ <p>Text</p> } icon={ this.iconHome() } />
+    )
+  }
+
   render() {
     return (
       <div className="BottomNav">
         
         <BottomNavigation showLabels={ true }>
-          <BottomNavigationAction label={ <p>Text</p> } icon={ iconHome } />
+          { this.homeNavigationButton() }
         </BottomNavigation>
         
       </div>
--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import './styles/index.css'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { ThemeProvider } from '@material-ui/styles'
+import theme from './theme'
 
 ReactDOM.render(
-  <App />,
+  <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+  </ThemeProvider>,
   document.getElementById('root')
 )
