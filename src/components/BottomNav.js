260105fc68ebcbd31353785b19ccf9ed738bdb69
Glitch (create-react-app-sample) <none>
2019-04-09 18:34:14 +0000

    🏟🐄 Checkpoint ./src/components/BottomNav.js:1424713/297

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,15 +1,20 @@
 import BottomNavigation from '@material-ui/core/BottomNavigation'
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
+import Icon from '@material-ui/core/Icon'
 
 import React, { Component } from 'react'
+
+const iconHome = (
+  <Icon fontSize={ "large" }>home</Icon>
+)
 
 class BottomNav extends Component {
   render() {
     return (
       <div className="BottomNav">
         
-        <BottomNavigation>
-          <BottomNavigationAction label={ <p>Text</p> } />
+        <BottomNavigation showLabels={ true }>
+          <BottomNavigationAction label={ <p>Text</p> } icon={ iconHome } />
         </BottomNavigation>
         
       </div>
