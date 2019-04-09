9d81d91621a83002a1a42210fdabded2dc867410
Glitch (fifth-process) <none>
2019-04-09 19:07:04 +0000

    🏇🍱 Checkpoint ./src/components/BottomNav.js:1424713/1198

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,19 +1,28 @@
 import BottomNavigation from '@material-ui/core/BottomNavigation'
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
-import Icon from '@material-ui/core/Icon'
-import HomeIcon from '@material-ui/icons/Home'
+// import Icon from '@material-ui/core/Icon'
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationIcon from '@material-ui/icons/LocationOn';
 
 import React, { Component } from 'react'
 
-class BottomNav extends Component {
-  
+const [value, setValue] = React.useState('home')
 
+function handleChange(event, newValue) {
+  setValue(newValue)
+}
+
+class BottomNav extends Component {
+  state = { value: "restore" }
   render() {
     return (
       <div className="BottomNav">
         
-        <BottomNavigation showLabels={ true }>
-          <BottomNavigationAction label="Home" icon={ <HomeIcon /> } />
+        <BottomNavigation value={ value } showLabels={ true } styles={{ width: 400 }} onChange={ handleChange }>
+          <BottomNavigationAction label="Restore" value="restore" icon={ <RestoreIcon /> } />
+          <BottomNavigationAction label="Favorite" value="favorite" icon={ <FavoriteIcon /> } />
+          <BottomNavigationAction label="Location" value="location" icon={ <LocationIcon /> } />
         </BottomNavigation>
         
       </div>
