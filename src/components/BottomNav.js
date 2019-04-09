1d428a4925d48939e52507dbc407a2f5468ffd4a
Glitch (fifth-process) <none>
2019-04-09 18:56:13 +0000

    🛍🍤 Checkpoint ./src/components/BottomNav.js:1424713/666

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,32 +1,19 @@
 import BottomNavigation from '@material-ui/core/BottomNavigation'
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
 import Icon from '@material-ui/core/Icon'
+import HomeIcon from '@material-ui/icons/Home'
 
 import React, { Component } from 'react'
 
-const iconHome = (
-  <Icon color={ "primary" }>home</Icon>
-)
-
 class BottomNav extends Component {
-  iconHome() {
-    return (
-      <Icon color={ this.props.active === "home" ? "primary" : "default" }>home</Icon>
-    )
-  }
-
-  homeNavigationButton() {
-    return (
-      <BottomNavigationAction label={ <p>Text</p> } icon={ this.iconHome() } />
-    )
-  }
+  
 
   render() {
     return (
       <div className="BottomNav">
         
         <BottomNavigation showLabels={ true }>
-          { this.homeNavigationButton() }
+          <BottomNavigationAction label="Home" icon={ <HomeIcon /> } />
         </BottomNavigation>
         
       </div>
