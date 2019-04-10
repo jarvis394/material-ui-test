7ee489d6de02cc316e9215662f4d702701db5014
Glitch (fifth-process) <none>
2019-04-10 10:22:37 +0000

    👓🎑 Checkpoint ./src/index.js:1424713/102 ./public/index.html:1424713/73 ./src/components/App.js:1424713/184

--- src/index.js
+++ src/index.js
@@ -4,15 +4,17 @@ import ReactDOM from 'react-dom'
 import { App } from './components/App'
 
 import './styles/index.css'
-
+/*
 import { CssBaseline } from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/styles'
-import { Theme } from './theme'
+import { Theme } from './theme'*/
 
-ReactDOM.render(
+/*ReactDOM.render(
   <ThemeProvider theme={ Theme }>
     <CssBaseline />
     <App />
   </ThemeProvider>,
   document.getElementById('root')
-)
+)*/
+
+ReactDOM.render(<App />, document.getElementById('root'))
