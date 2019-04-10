cd7610fb66dcde9daa067d9d6098cb7cce27e570
Glitch (fifth-process) <none>
2019-04-10 10:55:46 +0000

    💘🏨 Checkpoint ./src/components/BottomNav.js:1424713/4

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -3,10 +3,10 @@ import { BottomNavigationAction } from '@material-ui/core/BottomNavigationAction
 
 import { makeStyles } from '@material-ui/core/styles'
 
-import RestoreIcon from '@material-ui/icons/Restore'
+/*import RestoreIcon from '@material-ui/icons/Restore'
 import FavoriteIcon from '@material-ui/icons/Favorite'
 import LocationIcon from '@material-ui/icons/LocationOn'
-
+*/
 import React, { Component, useState } from 'react'
 
 const styles = makeStyles({
