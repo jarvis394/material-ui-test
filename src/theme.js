b2c29e017fe0daf38ca8e60633760b25a9ab3499
Glitch (fifth-process) <none>
2019-04-10 12:31:59 +0000

    😎👚 Checkpoint ./src/theme.js:1424713/56

--- src/theme.js
+++ src/theme.js
@@ -5,7 +5,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: '#556cd6',
+      main: '#2196F3',
     },
     secondary: {
       main: '#19857b',
@@ -14,7 +14,7 @@ const theme = createMuiTheme({
       main: red.A400,
     },
     background: {
-      default: '#fff',
+      default: '#aaa',
     },
   },
 });
