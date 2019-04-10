3e3e96cd7a5c66366a0d1bbe2cf6f2f8dd2c0a87
Glitch (fifth-process) <none>
2019-04-10 13:57:27 +0000

    🏖🚜 Checkpoint ./public/index.html:1424713/12 ./public/manifest.json:1424713/12 ./src/components/App.js:1424713/95

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react'
 import '../styles/App.css'
 
+import Button from '@material-ui/core/Button'
+
 import BottomNav from './BottomNav'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        
+        <Button >Text</Button>
         
         <BottomNav />
         
