6dacb7fa28ab141322e7560682db9a8357aa6165
Glitch (fifth-process) <none>
2019-04-10 12:42:24 +0000

    👦😚 Checkpoint ./src/components/BottomNav.js:1424713/333

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -2,8 +2,6 @@ import BottomNavigation from '@material-ui/core/BottomNavigation'
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
 import Paper from '@material-ui/core/Paper'
 
-// import makeStyles from '@material-ui/core/styles'
-
 import RestoreIcon from '@material-ui/icons/Restore'
 import FavoriteIcon from '@material-ui/icons/Favorite'
 import LocationIcon from '@material-ui/icons/LocationOn'
@@ -29,7 +27,7 @@ class BottomNav extends Component {
   
   render() {
     return (
-      <Paper className="Navigation" style={ styles.root }>
+      <Paper style={ styles.root }>
         <BottomNavigation 
           value={ this.state.active } 
           showLabels={ false } 
