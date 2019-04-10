d1c54c9528ec80d0f93cbe60062c8c5535d237e3
Glitch (fifth-process) <none>
2019-04-10 10:51:27 +0000

    Rewound to commit 260105fc68ebcbd31353785b19ccf9ed738bdb69 + 0 edits

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,27 +1,20 @@
 import BottomNavigation from '@material-ui/core/BottomNavigation'
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
-// import Icon from '@material-ui/core/Icon'
-import RestoreIcon from '@material-ui/icons/Restore';
-import FavoriteIcon from '@material-ui/icons/Favorite';
-import LocationIcon from '@material-ui/icons/LocationOn';
+import Icon from '@material-ui/core/Icon'
 
 import React, { Component } from 'react'
 
-const [value, setValue] = React.useState('home')
-
-function handleChange(event, newValue) {
-  setValue(newValue)
-}
+const iconHome = (
+  <Icon fontSize={ "large" }>home</Icon>
+)
 
 class BottomNav extends Component {
   render() {
     return (
       <div className="BottomNav">
         
-        <BottomNavigation value={ value } showLabels={ true } styles={{ width: 400 }} onChange={ handleChange }>
-          <BottomNavigationAction label="Restore" value="restore" icon={ <RestoreIcon /> } />
-          <BottomNavigationAction label="Favorite" value="favorite" icon={ <FavoriteIcon /> } />
-          <BottomNavigationAction label="Location" value="location" icon={ <LocationIcon /> } />
+        <BottomNavigation showLabels={ true }>
+          <BottomNavigationAction label={ <p>Text</p> } icon={ iconHome } />
         </BottomNavigation>
         
       </div>
--- src/index.js
+++ src/index.js
@@ -2,14 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/App'
 import './styles/index.css'
-import CssBaseline from '@material-ui/core/CssBaseline'
-import { ThemeProvider } from '@material-ui/styles'
-import theme from './theme'
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
+  <App />,
   document.getElementById('root')
 )
