efe032bcf4c4d4732912aa174c0d85d6cd918b6b
Glitch (fifth-process) <none>
2019-04-10 11:28:52 +0000

    👑🐘 Checkpoint ./src/components/BottomNav.js:1424713/822

--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -1,48 +1,52 @@
 import BottomNavigation from '@material-ui/core/BottomNavigation'
 import BottomNavigationAction from '@material-ui/core/BottomNavigationAction'
 import Icon from '@material-ui/core/Icon'
+import Paper from '@material-ui/core/Paper'
 
-//import { makeStyles } from '@material-ui/core/styles'
+// import makeStyles from '@material-ui/core/styles'
 
-/*import RestoreIcon from '@material-ui/icons/Restore'
+import RestoreIcon from '@material-ui/icons/Restore'
 import FavoriteIcon from '@material-ui/icons/Favorite'
 import LocationIcon from '@material-ui/icons/LocationOn'
-*/
 
 import React, { Component } from 'react'
 
-/* const styles = makeStyles({
+
+const styles = {
   root: {
     width: '100%',
     position: 'fixed',
     bottom: 0
   }
-})*/
+}
+
+//const [ value, setValue ] = React.useState('restore')
+
+    /*function handleChange(event, newValue) {
+      setValue(newValue)
+    }*/
 
 class BottomNav extends Component {
   render() {
-   /* const classes = styles()
-    const [ value, setValue ] = useState('restore')
-
-    function handleChange(event, newValue) {
-      setValue(newValue)
-    }
+    // const classes = styles()
+    
     
     return (
       <div className="Navigation">
         
         <BottomNavigation 
-          value={ value } 
-          className={ classes.root }
-          showLabels={ true } 
-          onChange={ handleChange }
+          value={ "restore" } 
+          style={ styles.root }
+          showLabels={ false } 
+          // onChange={ handleChange }
         >
+          <Paper>
           <BottomNavigationAction 
             label={"Restore" }
-           // value="restore" 
-            icon={ <Icon>restore</Icon> } 
+            value="restore" 
+            icon={ <RestoreIcon /> } 
           />
-          { /*<BottomNavigationAction 
+          <BottomNavigationAction 
             label="Favorite" 
             value="favorite" 
             icon={ <FavoriteIcon /> } 
@@ -51,9 +55,9 @@ class BottomNav extends Component {
             label="Location" 
             value="location" 
             icon={ <LocationIcon /> } 
-          /> */ }
+          /> 
+          </Paper>
         </BottomNavigation>
-        
       </div>
     )
   }
