f37301882d700385303a77418ce18251d3f7828d
Glitch (fifth-process) <none>
2019-04-10 12:53:23 +0000

    🏊🏼👂 Checkpoint ./src/theme.js:1424713/4 ./src/index.js:1424713/98 ./public/index.html:1424713/532 ./public/manifest.json:1424713/391

--- src/index.js
+++ src/index.js
@@ -1,16 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import CssBaseline from '@material-ui/core/CssBaseline';
-import { ThemeProvider } from '@material-ui/styles';
-import theme from './theme';
+import CssBaseline from '@material-ui/core/CssBaseline'
+import { ThemeProvider } from '@material-ui/styles'
+import theme from './theme'
 
 import App from './components/App'
 import './styles/index.css'
 
 ReactDOM.render(
-  <ThemeProvider theme={theme}>
-    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+  <ThemeProvider theme={ theme }>
     <CssBaseline />
     <App />
   </ThemeProvider>,
